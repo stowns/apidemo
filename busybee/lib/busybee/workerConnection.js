2ffe30a1c960055bb3c60127588d4efedb9f300b
Simon Townsend <stowns3@gmail.com>
2013-09-04 19:57:09 -0500

    removed busybee from project

--- busybee/lib/busybee/workerConnection.js
+++
@@ -1,9 +0,0 @@
-var zmq = require('zmq'),
-    WorkerConn;
-
-WorkerConn = module.exports = function (port) {
-  this.responder = zmq.socket('rep');
-  console.log('Worker Connecting to:' + port);
-  this.responder.connect(port);
-}
-
